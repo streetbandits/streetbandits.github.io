fe9581f0b32e782b56ce99dc6430f33281d676b2
StreetBandits UK <99140361+streetbandits@users.noreply.github.com>
2022-08-29 21:37:47 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ var body = document.getElementById('site_body');
 
 function loadSite() {
     audio.play();
-    body.innerHTML = `<center><middle><h1>Matt</h1>You ain't bad, stop actin'. &trade;
+    body.innerHTML = `<center><middle><h1>prmze (streetbandit)</h1>You ain't bad, stop actin'. &trade;
     <nav>
         <navItem><a href="https://instagram.com/prmze">Instagram</a></navItem>
         <navItem><a href="https://github.com/streetbandits">GitHUB</a></navItem>
