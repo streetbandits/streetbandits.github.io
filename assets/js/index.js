5ad1c160dddbb08c88ffeb03ba30b5b4e4b0a10c
prmze (streetbandit) <99140361+streetbandits@users.noreply.github.com>
2022-08-29 21:55:42 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,10 @@ var body = document.getElementById('site_body');
 
 function loadSite() {
     audio.play();
-    body.innerHTML = `<center><middle><h1>prmze (streetbandit)</h1>You ain't bad, stop actin'. &trade;
+    body.innerHTML = `<center><middle><h1>prmze</h1>You ain't bad, stop actin'. &trade;
     <nav>
         <navItem><a href="https://instagram.com/prmze">Instagram</a></navItem>
         <navItem><a href="https://github.com/streetbandits">GitHUB</a></navItem>
-        <navItem><a href="https:/github.com/streetbandits/kestral">Download Kestral</a></navItem>
+        <navItem><a href="https://github.com/streetbandits/kestral">Download Kestral</a></navItem>
     </nav></middle></center>`;
 }
